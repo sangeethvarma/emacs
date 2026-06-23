c03bfa47ba7376d5749e45195f37251ceea73940
Sangeeth <sangeeth.s.varma@gmail.com>
2026-06-23 00:03:38 +0000

    added log file viewing for backup

--- san-modules/san-help.el
+++ san-modules/san-help.el
@@ -12,4 +12,11 @@
 	 ("C-h F" . helpful-function)
 	 ("C-c C-d" . helpful-at-point)))
 
+(defun san/view-backup-log ()
+  "Instantly view the nightly encrypted backup log execution stream."
+  (interactive)
+  (find-file-other-window "~/.config/logs/backup.log"))
+
+(keymap-global-set "C-c h b" 'san/view-backup-log)
+
 (provide 'san-help)
